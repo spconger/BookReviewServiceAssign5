185aa7899aeb961d3b1a47df8a8021a3059b1f16
Steve Conger <steve@spconger.com>
2017-02-15 12:07:29 -0800

    Commit appcode files

---
+++ App_Code/BRService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "BRService" in code, svc and config file together.
+public class BRService : IBRService
+{
+    BookReviewDbEntities bre = new BookReviewDbEntities();
+    public int BookReviewLogin(string user, string password)
+    {
+        int key = 0;
+        int result = bre.usp_ReviewerLogin(user, password);
+        if(result != -1)
+        {
+            var ky = (from r in bre.Reviewers
+                      where r.ReviewerUserName.Equals(user)
+                      select r.ReviewerKey).FirstOrDefault();
+            key = (int)ky;
+           }
+
+        return key;
+    }
+
+    public List<Book> GetBooks()
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Review> GetReviews(int reviewerKey)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool NewReview(Review rev)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool RegisterReviewer(ReviewerLite r)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ App_Code/IBRService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IBRService" in both code and config file together.
+[ServiceContract]
+public interface IBRService
+{
+    [OperationContract]
+    int BookReviewLogin(string user, string password);
+
+    [OperationContract]
+    bool RegisterReviewer(ReviewerLite r);
+
+    [OperationContract]
+    bool NewReview(Review rev);
+
+    [OperationContract]
+    List<Review> GetReviews(int reviewerKey);
+
+    [OperationContract]
+    List<Book> GetBooks();
+
+}
+[DataContract]
+public class ReviewerLite
+{
+    [DataMember]
+    public string Lastname { set; get; }
+
+    [DataMember]
+    public string Firstname { set; get; }
+
+    [DataMember]
+    public string Email { set; get; }
+
+    [DataMember]
+    public string Username { set; get; }
+
+    [DataMember]
+    public string Password { set; get; }
+
+}
